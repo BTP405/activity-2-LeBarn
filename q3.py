0e8071a1496081975cd68fb3cb9e1b6d13dd116c
LeBarn <105302036+LeBarn@users.noreply.github.com>
2024-02-15 00:59:21 +0000

    q3 ansA

--- q3.py
+++ q3.py
@@ -1,1 +1,17 @@
 def wordCount(t):
+text = open("sample.txt", "r") #open text file
+d = dict() #make a dictoionary as the question requires
+for line in text: 
+     line = line.strip() 
+     words = line.split(" ")
+
+     #start for each loop
+     for word in words: 
+         if word in d: #does this word already exist in the dictionary?
+              d[word] = d[word] + 1
+        else: 
+            d[word] = 1 # put new word into dictoionary
+  
+
+for key in list(d.keys()): #print out the  key and value
+    print(key, ":", d[key]) 
